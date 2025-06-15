084ff84da854f56e2e2fa6e7671881c0ba98a6d5
Ana Tatar <94306011+anatatar10@users.noreply.github.com>
2025-06-15 12:32:55 +0300

    Add files

--- ai_models/src/count_final_images.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-IMG_ROOT = Path("/Users/anatatar/Desktop/Licenta/ai_models/data/ecg_images_final")
-png_files = list(IMG_ROOT.rglob("*.png"))
-
-print(f"✅ Total .png images found: {len(png_files)}")
--- ai_models/src/read_ptbxl_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dataset_path = "/Users/anatatar/Desktop/Licenta/ai_models/data/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.3/ptbxl_database.csv"
-
-df = pd.read_csv(dataset_path)
-
-# print(df.info()) # columns and types
-print(df.head().to_string())
